379e65b1c8ff3bd14b6d6ddb8d09026eb21fb3c7
Melissa Cabral <melissacabral@gmail.com>
2015-08-31 08:03:08 -0700

    adding functions

--- functions.php
+++ functions.php
@@ -4,8 +4,47 @@
 //activate "featured images" for posts
 add_theme_support( 'post-thumbnails' );
 
+//background color and image color
+add_theme_support( 'custom-background' );
+
+//use HTML5 instead of XHTML 1 on wordpress-generated code
+add_theme_support( 'html5', array( 'search-form', 'comment-list', 'comment-form', 
+									'gallery', 'caption' ) );
+
+//makes the <title> more accurate, SEO friendly, and customizable
+//don't forget wp_title() in the header file
+add_theme_support( 'title-tag' );
+
+//add RSS <link> tags to every screen
+add_theme_support( 'automatic-feed-links' );
+
+add_theme_support( 'post-formats', array('image', 'gallery', 'audio', 'quote', 
+											'video', 'chat') );
+
 //add a special image size just for the front page banner
 //				   name 	width  height  crop?
 add_image_size( 'big-banner', 1065, 250, true );
+
+//adds the ability to have editor-style.css to make the editor easier to use
+add_editor_style();
+
+/**
+ * Make excerpts better by changing the length and [...]
+ * @since  0.1
+ */
+function awesome_excerpt_length(){
+	//short excerpts on the search results, longer excerpts everywhere else
+	if( is_search() ){
+		return 20; //words
+	}else{
+		return 70; //words
+	}
+}
+add_filter( 'excerpt_length', 'awesome_excerpt_length' );
+
+function awesome_readmore(){
+	return ' <a href="' . get_permalink() . '" class="readmore">Read More</a>';
+}
+add_filter( 'excerpt_more', 'awesome_readmore' );
 
 //no close php
--- header.php
+++ header.php
@@ -3,7 +3,9 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>" />
 	<meta name="viewport" content="width=device-width" />
-	<title><?php bloginfo('name'); ?> - <?php bloginfo('description'); ?></title>
+
+	<title><?php wp_title( '|', true, 'right' ); ?></title>
+	
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
 	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_directory' ); ?>/styles/reset.css" />
